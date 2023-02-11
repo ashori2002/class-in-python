c8e63be5b6d387233d6e0c1b5df727ac29113810
ashori2002 <mohammadashori2002@gmail.com>
2023-02-12 00:11:47 +0330

    __init__ Receive input by function __init__

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 class Car:
-    def __init__(self):
-        print("this is init method")
+    def __init__(self , name):
+        print(f"my car's name {name}")
     def test(self):
         print("test")
 
-pride = Car()
+pride = Car("pride 111")
