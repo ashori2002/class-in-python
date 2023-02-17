1b181c42f2adf8cd36c23ff5f39ce8c8998efd45
ashori2002 <mohammadashori2002@gmail.com>
2023-02-17 16:37:18 +0330

    Inheritance in Python

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-class Car:
-    def __init__(self , n):
-        self.name = n
-        print(f"my car's name {n}")
-    def test(self):
-        print(f"test {self.name}")
+# class Car:
+#     def __init__(self , n):
+#         self.name = n
+#         print(f"my car's name {n}")
+#     def test(self):
+#         print(f"test {self.name}")
+#
+# pride = Car("pride 111")
 
-pride = Car("pride 111")
+#____________________________________________________________
+
+class A:
+    def printA(self):
+        print("Inheritance in Python A")
+class B(A):
+    def printB(self):
+        print("Inheritance in Python B")
+
+x = B()
+x.printA()
